fae580a8ca9336a89da3297e6aa5bc12cecebab3
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 19:38:35 +0530

    Create Day4forloop1.java

---
+++ Day4forloop1.java
@@ -0,0 +1,14 @@
+Day4forloop1.java
+  package day4;
+import java.util.Scanner;
+public class Loop2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num=10;
+		while(num>=0)
+		{
+			System.out.println(num);
+			num--;			
+		}
+	}
+}
