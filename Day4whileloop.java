0eb6b98701e52fb6cce9ba989f0dd09123f5f757
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 20:06:11 +0530

    Create Day4whileloop.java

---
+++ Day4whileloop.java
@@ -0,0 +1,12 @@
+Day4whileloop.java
+  package day4;
+public class reverse1 {
+	public static void main(String[] args) {
+		int num = 10;
+		while(num>0)
+		{
+			System.out.println(num);
+			num--;
+		}
+	}
+}
