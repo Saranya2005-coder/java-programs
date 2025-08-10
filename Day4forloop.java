a2cc041ebf0cbc4b998c6deee70811fefeeac998
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 19:36:03 +0530

    Create Day4forloop.java

---
+++ Day4forloop.java
@@ -0,0 +1,11 @@
+Day4forloop.java
+  package day4;
+public class loop {
+	public static void main(String[] args) {
+		int num=10;
+		for(int i=1;i<=num;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
