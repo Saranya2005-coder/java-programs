996151e310c599a0129f41f6c9659520abf0c589
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 12:00:04 +0530

    Create Datatype2.java

---
+++ Datatype2.java
@@ -0,0 +1,10 @@
+Datatype2.java
+  package Day1;
+public class Bsicprg3 {
+	public static void main(String[] args) {
+		byte num  =6;
+		byte num2 = 8;
+		byte num3 = (byte)(num + num2);
+		System.out.println(num3);
+	}
+}
