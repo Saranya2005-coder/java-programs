ef34622178c75b0af1360db7941374c0e53d33b3
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 20:00:49 +0530

    Create Day2shiftoperator.java

---
+++ Day2shiftoperator.java
@@ -0,0 +1,9 @@
+Day2shiftoperator.java
+  package college;
+public class Shiftoperator {
+	public static void main(String[] args) {
+    int a=50;
+    int b=20;
+    System.out.println(a>>b);
+	}
+}
