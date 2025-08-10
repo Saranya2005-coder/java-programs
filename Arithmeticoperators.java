7aeaa7f4cb99621476c68cdee74a348cbd289bf9
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 17:48:13 +0530

    Create Arithmeticoperators.java

---
+++ Arithmeticoperators.java
@@ -0,0 +1,12 @@
+Arithmeticoperators.java
+  package Day2;
+public class Arithmetic {
+	public static void main(String[] args) {
+		int num1 = 4;
+		int num2 = 2;
+		int num3 = num1 % num2;
+		int num4 = num1 / num2;
+		System.out.println(num3);
+		System.out.println(num4);
+	}
+}
