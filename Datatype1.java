80ee7e2b83ef39617cede2ae75d453b0a6d42f75
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 11:54:49 +0530

    Create Datatype1.java

---
+++ Datatype1.java
@@ -0,0 +1,9 @@
+Datatype1.java
+  package Day1;
+public class Basicprg2 {
+	public static void main(String[] args) {
+		float num3 = 6.6f;
+		int num4 = (int)num3;
+		System.out.println(num4);
+	}
+}
