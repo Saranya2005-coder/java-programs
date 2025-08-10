6039d89aad725767d69ec7f567e25c4153c74a01
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 20:22:33 +0530

    Create Dayevenindex.java

---
+++ Dayevenindex.java
@@ -0,0 +1,21 @@
+Dayevenindex.java
+  package day5;
+import java.util.Scanner;
+public class Evenindex {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int arr[] = new int[size];
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i] = sc.nextInt(); 
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(arr[i]);
+			}
+		}
+	}
+}
