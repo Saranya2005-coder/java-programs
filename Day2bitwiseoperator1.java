c0df159522b60920d33706f99f02f1a34b4cb3cd
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 18:11:02 +0530

    Create Day2bitwiseoperator1.java

---
+++ Day2bitwiseoperator1.java
@@ -0,0 +1,10 @@
+Day2bitwiseoperator1.java
+  package Day2;
+import java.util.Scanner;
+public class Bitwise1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 9656;
+		System.out.println(num%1000);
+	}
+}
