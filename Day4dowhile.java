f274027a4b1e2b8bb8e744ba5304d4b18db1e1ee
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 20:12:16 +0530

    Create Day4dowhile.java

---
+++ Day4dowhile.java
@@ -0,0 +1,13 @@
+Day4dowhile.java
+  package day4;
+import java.util.Scanner;
+public class Dwhile {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 10;
+		do {
+	        System.out.println(num);
+			num++;
+		}while(num!=11);
+	}
+}
