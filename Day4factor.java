38fcd3b549d3ba3dc1f599e4390411bec0ba9c6c
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-10 19:47:02 +0530

    Create Day4factor.java

---
+++ Day4factor.java
@@ -0,0 +1,16 @@
+Day4factor.java
+  package day4;
+import java.util.Scanner;
+public class factor {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num =sc.nextInt();
+		for(int i=1;i<=num;i++)
+		{
+			if(num%i==0)
+			{
+				System.out.println(i);
+			}
+		}   
+	}
+}
