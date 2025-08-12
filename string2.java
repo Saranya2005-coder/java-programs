3d39855f18943332739f9f7a4cc145fc07683b04
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-12 20:24:10 +0530

    Create string2.java

---
+++ string2.java
@@ -0,0 +1,17 @@
+string2.java
+  package Day7;
+import java.lang.String;
+public class Sumofdigit2 {
+	public String display()
+	{
+		String name = "sara";
+		return name;
+	}
+	public static void main(String[] args) 
+	{
+		Sumofdigit2 obj = new Sumofdigit2();
+		String res = obj.display();
+		System.out.println(res);
+	}
+
+}
