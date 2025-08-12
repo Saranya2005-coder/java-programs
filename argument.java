3e1063938cb37079a2368a01fdb14884709fc186
Saranya2005-coder <saranya9493@mountzion.ac.in>
2025-08-12 20:22:11 +0530

    Create argument.java

---
+++ argument.java
@@ -0,0 +1,16 @@
+argument.java
+  package Day7;
+import java.util.Scanner;
+public class Argument 
+{
+	public void display()
+	{
+		System.out.println("Hi nice to meet you");
+	}
+	public static void main(String[] args)
+	{
+		Argument sc = new Argument();
+		obj.display("");
+	}
+
+}
